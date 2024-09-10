8376601c072763509f7533102440be45dc35780c
Mukesh Choudhary <mukeshchoudhary1062003@gmail.com>
2024-09-10 19:28:15 +0530

    import bug

--- Server/routes/Users.js
+++ Server/routes/Users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { fetchUserById, updateUser } = require("../controller/USer");
+const { fetchUserById, updateUser } = require("../controller/User");
 const router = express.Router();
 
 router.get("/own", fetchUserById).patch("/:id", updateUser);
